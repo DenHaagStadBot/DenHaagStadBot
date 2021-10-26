6a62f1497283d4e618bc99a51f036652df418c05
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-10-26 18:13:51 +0200

    Update StaffSuggestie.js

--- Commands/StaffSuggestie.js
+++ Commands/StaffSuggestie.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
     if (!message.member.roles.cache.has('682635913431482471')) return message.reply('> Alleen de server moderators kunnen ');
    
